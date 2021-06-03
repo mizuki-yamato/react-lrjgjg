9fe70f470192920b060706cce0796195e49022c8
mizuki-yamato <mizuki.yamato@gmail.com>
2021-06-03 11:32:53 +0900

    button atom created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import { PrimaryButton } from './components/atom/button/PrimaryButton';
+import { SecondaryButton } from './components/atom/button/SecondaryButton';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <PrimaryButton children="OK" />
+      <SecondaryButton children="NG" />
     </div>
   );
 }
---
+++ src/components/atom/button/BaseButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  color: #fff;
+  padding: 6px 24px;
+  border: none;
+  outlone: none;
+  border-radius: 9999px;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
